31c0b3750aa5cf29506d50eca5b066ff1165a6a7
kirit-pendyala <pendyalakirit@gmail.com>
2021-02-21 16:48:48 -0600

    Button

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,28 @@
-import React, {useState} from 'react'
-import {Link} from 'react-router-dom'
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import {Button} from './Button';
+import './Navbar.css';
 export default Navbar
 
 function Navbar() {
   const [click, setClick] = useState(false);
 
+  const [button, setButton] = useState(true)
   const handleClick = () => setClick(!click);
 
-  const closeMobileMenu = () => setClick(false);
+  const closeMobileMenu = () => setClick(false)
+  const showButton = () =>  {
+    if(window.innerWidth <= 960)
+    {
+      setButton(false);
+    }
+    else
+    {
+      setButton(true);
+    }
+  };
 
+  window.addEventListener('resize', showButton);
   return (
     <>
       <nav className="navbar">
@@ -26,8 +40,31 @@ function Navbar() {
                 Home
               </Link>
             </li>
+            <li className='nav-item'>
+              <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                Services
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                Products
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/sign-up'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                SignUp
+              </Link>
+            </li>
           </ul>
+          {button && <Button buttonStyle='btn--outline'> SIGN UP</Button>}
+
+
         </div>
       </nav>
     </>
-)}
+);
+}
